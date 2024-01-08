0a2622b6bf1b4c3b1699a1c7d04cdadab183624f
Muhammad Husni Fahmi Fuady <husni.fuad22@gmail.com>
2024-01-08 20:03:57 +0700

    Routing: Pages

--- my-first-app/src/routes/+page.svelte
+++ my-first-app/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit!</h1>
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<h1>Home</h1>
+<p>this is the home page.</p>
+
+<h2>Welcome to SvelteKit!</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
