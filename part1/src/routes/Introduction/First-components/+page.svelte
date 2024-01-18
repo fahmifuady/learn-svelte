04233bd4fbd009d00031239ed013faae875b8420
Muhammad Husni Fahmi Fuady <husni.fuad22@gmail.com>
2024-01-18 16:12:14 +0700

    Introduction: First-components

--- part1/src/routes/Introduction/First-components/+page.svelte
+++ part1/src/routes/Introduction/First-components/+page.svelte
@@ -3,3 +3,18 @@
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+
+<p>
+	In Svelte, an application is composed from one or more components. A component is a reusable
+	self-contained block of code that encapsulates HTML, CSS and JavaScript that belong together,
+	written into a .svelte file. The App.svelte file, open in the code editor to the right, is a
+	simple component.
+</p>
+
+<div>
+	code:
+	<br />
+	&lt;script&gt; export let name = &#39;Fufu&#39;; &lt;/script&gt;
+	<br />
+	&lt;h1&gt;Hello &lbrace; name.toUpperCase() &rbrace;!&lt;/h1&gt;
+</div>
