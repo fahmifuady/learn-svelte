5f9527004608fd75f49deeea376329d9466b58ec
Muhammad Husni Fahmi Fuady <husni.fuad22@gmail.com>
2024-01-18 16:17:36 +0700

    Dynamic-attributes

--- part1/src/routes/Introduction/Dynamic-attributes/+page.svelte
+++ part1/src/routes/Introduction/Dynamic-attributes/+page.svelte
@@ -4,3 +4,22 @@
 </script>
 
 <img {src} alt="{name} dances." />
+<br />
+<p>
+	Just like you can use curly braces to control text, you can use them to control element
+	attributes.
+</p>
+
+<div>
+	code:
+	<br />
+	&lt;script&gt;
+	<br />
+	import src from &#39;$lib/assets/image.gif&#39;;
+	<br />
+	let name = &#39;Rick Astley&#39;;
+	<br />
+	&lt;/script&gt;
+	<br />
+	&lt;img &lcub; src &rcub; alt=&quot;&lcub;name&rcub; dances.&quot; /&gt;
+</div>
