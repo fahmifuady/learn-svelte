a8d6997e6ded1b098e313b9e992358324f54b785
Muhammad Husni Fahmi Fuady <husni.fuad22@gmail.com>
2024-01-18 16:02:18 +0700

    Introduction: Styling

---
+++ part1/src/routes/Introduction/Styling/+page.svelte
@@ -0,0 +1,12 @@
+<p>
+	Just like in HTML, you can add a &lt;style&gt; tag to your component. Let&#39;s add some styles to
+	the &lt;p&gt; element:
+</p>
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
