c03ba7bc113a07db521bf33dcca0d03df4cf846a
Muhammad Husni Fahmi Fuady <husni.fuad22@gmail.com>
2024-01-18 16:21:33 +0700

    Introduction: Styling

--- part1/src/routes/Introduction/Styling/+page.svelte
+++ part1/src/routes/Introduction/Styling/+page.svelte
@@ -1,10 +1,23 @@
+<h1>This is Heading 1</h1>
+
 <p>
 	Just like in HTML, you can add a &lt;style&gt; tag to your component. Let&#39;s add some styles to
 	the &lt;p&gt; element:
 </p>
+<div>
+	code
+	<br />
+	&lt;h1&gt;This is Heading 1&lt;/h1&gt;
+	<br />
+	&lt;style&gt;
+	<br />
+	h1 &lcub; color: goldenrod; font-family: &#39;Comic Sans MS&#39;, cursive; font-size: 2em; &rcub;
+	<br />
+	&lt;/style&gt;
+</div>
 
 <style>
-	p {
+	h1 {
 		color: goldenrod;
 		font-family: 'Comic Sans MS', cursive;
 		font-size: 2em;
