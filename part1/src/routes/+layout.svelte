7fad6ce9c7681a1096644cf59d8f4d34fe90257c
Muhammad Husni Fahmi Fuady <husni.fuad22@gmail.com>
2024-01-18 15:19:06 +0700

    Introduction: Welcome to svelte

---
+++ part1/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/Introduction">Introduction</a>
+	<a href="/Reactivity">Reactivity</a>
+	<a href="/Props">Props</a>
+	<a href="/Logic">Logic</a>
+	<a href="/Events">Events</a>
+	<a href="/Bindings">Bindings</a>
+	<a href="/Lifecycle">Lifecycle</a>
+	<a href="/Stores">Stores</a>
+</nav>
+<slot />
---
+++ part1/src/routes/Introduction/+layout.svelte
@@ -0,0 +1,10 @@
+<nav>
+	<a href="/Introduction/">Welcome to Svelte</a>
+	<a href="/Introduction/First-component">First-component</a>
+	<a href="/Introduction/Dynamic-attributes">Dynamic-attributes</a>
+	<a href="/Introduction/Styling">Styling</a>
+	<a href="/Introduction/Nested-components">Nested-components</a>
+	<a href="/Introduction/HTML-tags">HTML-tags</a>
+</nav>
+
+<slot />
